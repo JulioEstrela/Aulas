21d482a7b42fb492bd57beb6982c3d17c3b93ba4
Julio <97416841+JulioEstrela@users.noreply.github.com>
2023-03-09 11:31:46 -0300

    Create TicTacToe.java

---
+++ TicTacToe.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+
+public class tictac
+{
+    public static void main(String[] args)
+    {
+        char[][] board = new char[3][3];
+
+        for (int i = 0; i < board.length; i++) {
+            Arrays.fill(board[i], '.');
+        }
+
+        /*while (!victory(board) || !draw())
+        {
+            ShowBoard(board);
+        }*/
+
+        ShowBoard(board);
+
+
+    }
+
+    public static boolean victory(char[][] board)
+    {
+        for (char[] line: board)
+        {
+            for (char position: line)
+            {
+
+            }
+        }
+
+        if (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X')
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static  boolean draw()
+    {
+        return  false;
+    }
+
+    public static void ShowBoard(char[][] board)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
