e7ebdb2b98ce0b697e6d2af2843a6dabda0352bb
Julio <97416841+JulioEstrela@users.noreply.github.com>
2023-05-11 17:33:01 -0300

    Update Loja.java

--- Segundo Bimestre/Loja/Loja.java
+++ Segundo Bimestre/Loja/Loja.java
@@ -1,8 +1,39 @@
+import java.util.List;
+
 public class Loja 
 {
-    private String[] nomeDaLoja;
-    private Cliente[] clientes;
-    private Pedido[] pedidos;
+    private String nomeDaLoja;
+    private List<Cliente> clientes;
+    private List<Pedido> pedidos;
 
+    public Loja(){}
+
+    public Loja(String nomeDaLoja)
+    {
+        this.nomeDaLoja = nomeDaLoja;
+    }
+
+    void AddCliente(Cliente cliente)
+    {
+        clientes.add(cliente);;
+    }
+
+    void AddPedido(Pedido pedido)
+    {
+        pedidos.add(pedido);
+    }
     
+    //#region Getters
+    public String getNomeDaLoja() {
+        return nomeDaLoja;
+    }
+
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+    //#endregion
 }
