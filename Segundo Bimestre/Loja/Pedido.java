ae76c78eb5716a9acf2a1bc24d4025ff9503b341
Julio <97416841+JulioEstrela@users.noreply.github.com>
2023-05-11 17:33:33 -0300

    Update Pedido.java

--- Segundo Bimestre/Loja/Pedido.java
+++ Segundo Bimestre/Loja/Pedido.java
@@ -5,9 +5,30 @@ public class Pedido
     private float valor;
     private String codigo;
 
-    public Pedido()
+    public Pedido(Cliente cliente, String produto, float valor, String codigo)
     {
-
+        this.cliente = cliente;
+        this.produto = produto;
+        this.valor = valor;
+        this.codigo = codigo;
     }
+
+    //#region Getters
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public String getProduto() {
+        return produto;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+    //#endregion
 
 }
