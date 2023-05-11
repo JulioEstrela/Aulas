19f561ad6c8488d11002ca68701fa8cc94671ade
Julio <97416841+JulioEstrela@users.noreply.github.com>
2023-05-11 17:32:23 -0300

    Update Cliente.java

--- Segundo Bimestre/Loja/Cliente.java
+++ Segundo Bimestre/Loja/Cliente.java
@@ -4,13 +4,24 @@ public class Cliente{
     private String cpf;
     private String telefone;
 
-
     public Cliente(String nome, String cpf, String telefone)
     {
         this.nome = nome;
         this.cpf = cpf;
         this.telefone = telefone;
     }
-    
 
+    //#region Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+    //#endregion
 }
