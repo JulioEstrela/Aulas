a167e4407060d0208f791ebcaa40c1f684cb70bf
Julio <97416841+JulioEstrela@users.noreply.github.com>
2023-06-14 23:33:44 -0300

    Add files via upload

---
+++ Segundo Bimestre/Empresa/model/Empresa.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.util.List;
+
+public class Empresa {
+    List<Item> itens;
+
+    void addItem(Item item)
+    {
+
+    }
+
+    void listarItens()
+    {
+
+    }
+
+    void comercializarItem(Long id)
+    {
+
+    }
+}
